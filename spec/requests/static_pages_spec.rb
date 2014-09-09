bbc07e2b9ec431662934fd6195e22d6cd1163170
7stud <7stud@excite.com>
2014-09-09 17:45:49 -0600

    Add StaticPages tests

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+# Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+
+describe "StaticPages" do
+
+  describe "Home page" do
+    it "should have content 'Sample App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
+      #response.status.should be(200)
+    end
+
+    it "should have title that includes 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Home")
+    end
+
+  end
+
+  describe "Help page" do
+    it "should contain the text 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+
+    it "should have title that includes 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title('Help')
+    end
+  end
+  
+  describe "About page" do
+    it "should have content 'About'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About')
+    end
+
+    it "should have title that includes 'About'" do
+      visit '/static_pages/about'
+      expect(page).to have_title('About')
+    end
+
+  end
+end
